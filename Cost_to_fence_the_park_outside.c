4783c4db2f51f54cb198f3da000f326c7490a1a0
Triveni7676 <98643153+Triveni7676@users.noreply.github.com>
2022-02-12 11:40:03 +0530

    #include<stdio.h> main() {     int l,b,w,c,area,cost;     scanf("%d%d%d%d",&l,&b,&w,&c);     area=((l+(2*w))*(b+(2*w)))-(l*b);     cost=area*c;     printf("%d",cost);     return 0;
    }

---
+++ Cost_to_fence_the_park_outside.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+    int l,b,w,c,area,cost;
+    scanf("%d%d%d%d",&l,&b,&w,&c);
+    area=((l+(2*w))*(b+(2*w)))-(l*b);
+    cost=area*c;
+    printf("%d",cost);
+    return 0;
+    
+}
