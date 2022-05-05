3da2bd561a633c9d84559455425e337d96708d16
Triveni7676 <98643153+Triveni7676@users.noreply.github.com>
2022-05-05 23:14:44 +0530

    #include<stdio.h> int fibonacci(int n) {     if(n==0)     {         return 0;     }     else if(n==1)     {         return 1;     }     else     {         return(fibonacci(n-1)+fibonacci(n-2));     } } int main() {     int n,i,res;     scanf("%d",&n);     for(i=0;i<n;i++)     {         res=fibonacci(i);         printf("%d ",res);     } }

---
+++ fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int fibonacci(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    else if(n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return(fibonacci(n-1)+fibonacci(n-2));
+    }
+}
+int main()
+{
+    int n,i,res;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        res=fibonacci(i);
+        printf("%d ",res);
+    }
+}
