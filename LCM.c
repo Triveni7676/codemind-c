c60e1bf13ecc9b9495c36023e6c6326b57ed904a
Triveni7676 <98643153+Triveni7676@users.noreply.github.com>
2022-05-05 22:47:36 +0530

    #include<stdio.h> int main() {     int a,b,t=2,lcm=1;     scanf("%d%d",&a,&b);     while(1)     {         if(a%t==0 && b%t==0)         {             a=a/t;             b=b/t;             lcm=lcm*t;         }         else         {             t++;         }         if(a<t || b<t)         {             break;         }
        }
        printf("%d",lcm*a*b);
    }

---
+++ LCM.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,t=2,lcm=1;
+    scanf("%d%d",&a,&b);
+    while(1)
+    {
+        if(a%t==0 && b%t==0)
+        {
+            a=a/t;
+            b=b/t;
+            lcm=lcm*t;
+        }
+        else
+        {
+            t++;
+        }
+        if(a<t || b<t)
+        {
+            break;
+        }
+      
+    }
+    printf("%d",lcm*a*b);
+}
