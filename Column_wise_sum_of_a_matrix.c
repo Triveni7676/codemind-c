8d4d0f7553541ede0a38fee9732db23cbcef85a1
Triveni7676 <98643153+Triveni7676@users.noreply.github.com>
2022-06-22 18:30:10 +0530

    #include<stdio.h> int main() { 	int i,j,k,r,a[10][10],c,sum,res=0; 	scanf("%d%d",&r,&c); //	scanf("%d%d",&r2,&c2); 	for(i=0;i<r;i++) 	{ 		for(j=0;j<c;j++) 		{ 			scanf("%d",&a[i][j]); 		} 	} 	for(j=0;j<c;j++) 	{ 	    sum=0; 		for(i=0;i<r;i++) 		{
    		       sum=sum+a[i][j];
    		}
    		printf("%d ",sum);
    	}
    	
    }

---
+++ Column_wise_sum_of_a_matrix.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,k,r,a[10][10],c,sum,res=0;
+	scanf("%d%d",&r,&c);
+//	scanf("%d%d",&r2,&c2);
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(j=0;j<c;j++)
+	{
+	    sum=0;
+		for(i=0;i<r;i++)
+		{
+		   
+		       sum=sum+a[i][j];
+		}
+		printf("%d ",sum);
+	}
+	
+}
+	
