2cba862442278a5ebe299a0f730591b951983135
Triveni7676 <98643153+Triveni7676@users.noreply.github.com>
2022-07-01 22:56:28 +0530

    #include<stdio.h> #include<math.h> int factcount(int i) {     int j,fc=2;     int sq=sqrt(i);     if(i==1)     {         return 1;     }     for(j=2;j<=sq;j++)     {         if(i%j==0)         {             fc+=2;         }     }     if(sq*sq==i)     {         fc-=1;     }     return fc; } int main() {     int i,n,m,fc,c=0;     scanf("%d",&n);     scanf("%d",&m);     for(i=n;i<=m;i++)     {         fc=factcount(i);         if(fc==2)         {             c++;         }     }     printf("%d",c); }

--- Primes_in_range.c
+++ Primes_in_range.c
@@ -1,20 +1,35 @@
 #include<stdio.h>
+#include<math.h>
+int factcount(int i)
+{
+    int j,fc=2;
+    int sq=sqrt(i);
+    if(i==1)
+    {
+        return 1;
+    }
+    for(j=2;j<=sq;j++)
+    {
+        if(i%j==0)
+        {
+            fc+=2;
+        }
+    }
+    if(sq*sq==i)
+    {
+        fc-=1;
+    }
+    return fc;
+}
 int main()
 {
-    int m,n,i,s,c=0;
-    scanf("%d%d",&m,&n);
-    for(m;m<=n;m++)
+    int i,n,m,fc,c=0;
+    scanf("%d",&n);
+    scanf("%d",&m);
+    for(i=n;i<=m;i++)
     {
-        s=0;
-        for(i=2;i*i<=m;i++)
-        {
-            if(m%i==0)
-            {
-                s=1;
-                break;
-            }
-        }
-        if(s==0 && m!=1)
+        fc=factcount(i);
+        if(fc==2)
         {
             c++;
         }
