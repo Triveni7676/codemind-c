6befc8672bad0b774236e554176d40cbd02ace5f
Triveni7676 <98643153+Triveni7676@users.noreply.github.com>
2022-07-02 00:19:03 +0530

    #include<stdio.h> int main() {     int a[100],i,j,k=0,c,n,s;   //  float s=0,avg;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&s);     for(i=0;i<n;i++)     {         c=1;         if(a[i]==-1)         {             continue;         }         for(j=i+1;j<n;j++)         {             if(a[i]==a[j])             {                 c++;                 a[j]=-1;             }         }         if(c==s)         {             k++;             printf("%d ",a[i]);            // s=s+a[i];         }     }     if(k==0)     {         printf("-1");     } }

---
+++ N_times_repeated_elements.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],i,j,k=0,c,n,s;
+  //  float s=0,avg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&s);
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        if(a[i]==-1)
+        { 
+            continue;
+        }
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+                a[j]=-1;
+            }
+        }
+        if(c==s)
+        {
+            k++;
+            printf("%d ",a[i]);
+           // s=s+a[i];
+        }
+    }
+    if(k==0)
+    {
+        printf("-1");
+    }
+}
