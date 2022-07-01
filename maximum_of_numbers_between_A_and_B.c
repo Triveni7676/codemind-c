da5e56a32fe411bb74ac933a28d2f700ba5e7ae1
Triveni7676 <98643153+Triveni7676@users.noreply.github.com>
2022-07-01 23:57:30 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,max=0,a,b,p=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]>=a && arr[i]<=b)         {             p++;             if(arr[i]>max)             {                 max=arr[i];             }         }     }     if(p!=0)     {     printf("%d",max);     //printf("%d",sum);     }     else     {         printf("-1");     } }

---
+++ maximum_of_numbers_between_A_and_B.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,max=0,a,b,p=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=a && arr[i]<=b)
+        {
+            p++;
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+    }
+    if(p!=0)
+    {
+    printf("%d",max);
+    //printf("%d",sum);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
